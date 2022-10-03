1255ca5f64c277716857bbb74f7805fb5b3885ae
trevor <63176386+TfedUD@users.noreply.github.com>
2022-10-03 12:40:28 -0400

    chore: initial commit

---
+++ honeybee_doe2/_extend_honeybee.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# -*- Python Version: 2.7 -*-
+"""This is called during __init__and extends the base honeybee class Properties with a new ._doe2 slot"""
+
+from honeybee.properties import (
+    ModelProperties,
+    RoomProperties,
+    FaceProperties,
+    ApertureProperties,
+    ShadeProperties
+)
+
