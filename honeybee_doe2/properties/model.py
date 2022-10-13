20c1ba02720741d41566552be7c4d03af2c02137
trevor <63176386+TfedUD@users.noreply.github.com>
2022-10-13 12:22:47 -0400

    feat: added model to inp in writer

--- honeybee_doe2/properties/model.py
+++ honeybee_doe2/properties/model.py
@@ -39,65 +39,6 @@ class ModelDoe2Properties(object):
     def _make_doe_stories(obj):
         pass
 
-    def to_inp_file(self):
-        data = [
-            self._header,
-            fb.global_params,
-            fb.ttrpddh, self.title.to_inp(), self.run_period.to_inp(),
-            fb.comply,
-            self.compliance_data.to_inp(),
-            self.site_bldg_data.to_inp(),
-            self.constructions.to_inp(),
-            fb.glzCode,
-            '\n'.join(gt.to_inp() for gt in self.glass_types),
-            fb.polygons,
-            '\n'.join(pl.to_inp() for pl in self.polygons),
-            fb.wallParams,
-            '\n'.join(shd.to_inp() for shd in self.context_shades),
-            fb.miscCost,
-            fb.perfCurve,
-            fb.floorNspace,
-            '\n'.join(flr.to_inp() for flr in self.floors),
-            fb.elecFuelMeter,
-            fb.elec_meter,
-            fb.fuel_meter,
-            fb.master_meter,
-            fb.hvac_circ_loop,
-            fb.pumps,
-            fb.heat_exch,
-            fb.circ_loop,
-            fb.chiller_objs,
-            fb.boiler_objs,
-            fb.dwh,
-            fb.heat_reject,
-            fb.tower_free,
-            fb.pvmod,
-            fb.elecgen,
-            fb.thermal_store,
-            fb.ground_loop_hx,
-            fb.comp_dhw_res,
-            fb.steam_cld_mtr,
-            fb.steam_mtr,
-            fb.chill_meter,
-            fb.hvac_sys_zone,
-            '\n'.join(hv_sys.to_inp() for hv_sys in self.hvac_system_zone),
-            fb.misc_meter_hvac,
-            fb.equip_controls,
-            fb.load_manage,
-            fb.big_util_rate,
-            fb.ratchets,
-            fb.block_charge,
-            fb.small_util_rate,
-            fb.output_reporting,
-            fb.loads_non_hrly,
-            fb.sys_non_hrly,
-            fb.plant_non_hrly,
-            fb.econ_non_hrly,
-            fb.hourly_rep,
-            fb.the_end
-        ]
-        return '\n\n'.join(data)
-
     def __str__(self):
         return "Model Doe2 Properties: [host: {}]".format(self.host.display_name)
 
