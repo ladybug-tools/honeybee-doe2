2337edc96bc23985a058e4d83d0382926a4698fd
trevor <63176386+TfedUD@users.noreply.github.com>
2022-10-31 10:46:54 -0400

    refactor: unhardcoding, geting 'end to end' framed

--- honeybee_doe2/writer.py
+++ honeybee_doe2/writer.py
@@ -1,9 +1,15 @@
 from properties.inputils import blocks as fb
-from honeybee.model import Model
-from properties.inputils.run_period import RunPeriod
+from properties.inputils.compliance import ComplianceData
+from progressbar.inputils.sitebldg import SiteBldgData as sbd
+from properties.inputils.run_period import RunPeriod as rp
 from properties.inputils.title import Title
 
+from honeybee.model import Model
+
 # Finally. Once again into the fray.
+
+comp_data = ComplianceData()  # TODO Un-Hardcode
+sb_data = sbd()  # TODO Un-Hardcode
 
 
 def model_to_inp(hb_model):
@@ -16,15 +22,16 @@ def model_to_inp(hb_model):
         Title(tile=str(hb_model.display_name)).to_inp(),
         rp.to_inp(),  # TODO unhardcode
         fb.comply,
-        hb_model.compliance_data.to_inp(),  # TODO remove from model properties
-        hb_model.site_bldg_data.to_inp(),  # TODO
-        hb_model.constructions.to_inp(),  # TODO
+        comp_data.to_inp(),
+        sb_data.to_inp(),
+        hb_model.constructions.to_inp(),  # TODO can reuse from hbdoe2
         fb.glzCode,
+        # TODO Glass types (might be hardcoded in OG)
         '\n'.join(gt.to_inp() for gt in hb_model.glass_types),
         fb.polygons,
         '\n'.join(pl.to_inp() for pl in hb_model.polygons),
         fb.wallParams,
-        '\n'.join(shd.to_inp() for shd in hb_model.context_shades),
+        '\n'.join(shd.to_inp() for shd in hb_model.context_shades),  # TODO shade support
         fb.miscCost,
         fb.perfCurve,
         fb.floorNspace,
@@ -51,7 +58,8 @@ def model_to_inp(hb_model):
         fb.steam_mtr,
         fb.chill_meter,
         fb.hvac_sys_zone,
-        '\n'.join(hv_sys.to_inp() for hv_sys in hb_model.hvac_system_zone),
+        '\n'.join(hv_sys.to_inp()
+                  for hv_sys in hb_model.hvac_system_zone),  # TODO need to frame up
         fb.misc_meter_hvac,
         fb.equip_controls,
         fb.load_manage,
