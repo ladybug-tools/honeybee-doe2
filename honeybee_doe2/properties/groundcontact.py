f70c58f63e1ab1837a040d98b54536fdba124bd7
mostaphaRoudsari <sadeghipour@gmail.com>
2023-05-02 21:26:09 -0400

    fix: manage the case for underground floors
    There are two main changes here:
    
    1. Reverse the order of vertices for floor and room vertices polygons
    2. Create a separate polygon for underground floors
    
    Generally speaking, I find the current implementation of writing the polygons separate from the main objects tricky. I suggest to move writing the DOE polygon into the `to_inp` method so you can ensure you pass the proper name to the method instead of assuming the name will be `display_name Plg`.

--- honeybee_doe2/properties/groundcontact.py
+++ honeybee_doe2/properties/groundcontact.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # -*- Python Version: 2.7 -*-
 from ..utils.doe_formatters import short_name
+from ..geometry.polygon import DoePolygon
 
 # ? Sooo equest is odd, but this is the way with having the undrgnd wall
 # *
@@ -9,19 +10,23 @@ from ..utils.doe_formatters import short_name
 class GroundFloor:
     def __init__(self, face):
         self.face = face
+        self.polygon = DoePolygon.from_face(face, underground_surface=True)
 
     def to_inp(self, space_origin):
 
         azimuth = 180 if self.face.azimuth == 0 else self.face.azimuth
         origin_pt = self.face.geometry.lower_left_corner - space_origin
 
-        obj_lines = []
+        # create a unique polygon for ground floor faces
+        polygon_name = f'{self.face.display_name}_ug Plg'
+        polygon = self.polygon.to_inp(name=polygon_name) + '\n'
+        obj_lines = [polygon]
         obj_lines.append(
             '"{}" = UNDERGROUND-WALL'.format(short_name(self.face.display_name)))
         obj_lines.append('\n  CONSTRUCTION = "{}_c"'.format(
             short_name(self.face.properties.energy.construction.display_name)))
         obj_lines.append('\n  LOCATION     = BOTTOM')
-        obj_lines.append('\n  POLYGON      = "{} Plg"'.format(self.face.display_name))
+        obj_lines.append('\n  POLYGON      = "{}"'.format(polygon_name))
         obj_lines.append('\n  AZIMUTH      = {}'.format(azimuth))
         obj_lines.append('\n  X            = {}'.format(origin_pt.x))
         obj_lines.append('\n  Y            = {}'.format(origin_pt.y))
