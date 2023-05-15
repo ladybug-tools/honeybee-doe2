28e3ff518710ba6279e6e28275c047af172b7be8
mostaphaRoudsari <sadeghipour@gmail.com>
2023-05-15 18:49:56 -0400

    fix(window): improve the grouping for apertures
    See here: https://github.com/ladybug-tools/honeybee-doe2/issues/20#issuecomment-1548712189

--- honeybee_doe2/properties/aperture.py
+++ honeybee_doe2/properties/aperture.py
@@ -92,12 +92,65 @@ class Window:
             else:
                 groups[min_2d.y] = [face]
 
+        # merge the rows if they have the same number of grids
+        sorted_groups = []
         for count, group in enumerate(groups.values()):
             # find min_2d and max_2d for each group
             group.sort(key=lambda x: vertices[x[0]].x)
             min_2d = vertices[group[0][0]]
             max_2d = vertices[group[-1][2]]
+            sorted_groups.append({'min': min_2d, 'max': max_2d})
 
+        def _get_last_row(groups, start=0):
+            """An internal function to return the index for the last row that can be
+            merged with the start row that is passed to this function.
+
+            This function compares the min and max x and y values for each row to see
+            if they can be merged into a rectangle.
+            """
+            for count, group in enumerate(groups[start:]):
+                next_group = groups[count + start + 1]
+                if abs(group['min'].x - next_group['min'].x) <= 0.01 \
+                    and abs(group['max'].x - next_group['max'].x) <= 0.01 \
+                        and abs(next_group['min'].y - group['max'].y) <= 0.01:
+                    continue
+                else:
+                    return start + count
+
+            return start + count + 1
+
+        # sort based on y
+        sorted_groups.sort(key=lambda x: x['min'].y)
+        merged_groups = []
+        start_row = 0
+        last_row = -1
+        while last_row < len(sorted_groups):
+            try:
+                last_row = _get_last_row(sorted_groups, start=start_row)
+            except IndexError:
+                merged_groups.append(
+                    {
+                        'min': sorted_groups[start_row]['min'],
+                        'max': sorted_groups[len(sorted_groups) - 1]['max']
+                    }
+                )
+                break
+            else:
+                merged_groups.append(
+                    {
+                        'min': sorted_groups[start_row]['min'],
+                        'max': sorted_groups[last_row]['max']
+                    }
+                )
+                if last_row == start_row:
+                    # the row was not grouped with anything else
+                    start_row += 1
+                else:
+                    start_row = last_row + 1
+
+        for count, group in enumerate(merged_groups):
+            min_2d = group['min']
+            max_2d = group['max']
             height = max_2d.y - min_2d.y
             width = max_2d.x - min_2d.x
             name = f'{short_name(self.aperture.display_name)}_{1000 + count}'
