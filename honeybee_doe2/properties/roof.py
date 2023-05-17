97cc276d7d949e55d8e11ab71a3db4eae63e21b3
mostaphaRoudsari <sadeghipour@gmail.com>
2023-05-17 13:24:15 -0400

    feat: add skylights

--- honeybee_doe2/properties/roof.py
+++ honeybee_doe2/properties/roof.py
@@ -1,5 +1,6 @@
 from ladybug_geometry.geometry3d.face import Face3D
 from ..utils.doe_formatters import short_name
+from .aperture import Window
 
 
 class DoeRoof:
@@ -15,15 +16,31 @@ class DoeRoof:
         azimuth = 180 if self.face.azimuth == 0 and tilt == 0 else self.face.azimuth
         origin_pt = self.face.geometry.lower_left_corner - space_origin
 
-        return \
-            '"{}" = ROOF'.format(p_name) + \
-            '\n  POLYGON           = "{}"'.format(p_name+' Plg') + \
-            '\n  CONSTRUCTION      = "{}_c"'.format(short_name(constr, 30)) + \
-            '\n  TILT              =  {}'.format(tilt) + \
-            '\n  AZIMUTH           =  {}'.format(azimuth) + \
-            '\n  X                 =  {}'.format(origin_pt.x) + \
-            '\n  Y                 =  {}'.format(origin_pt.y) + \
-            '\n  Z                 =  {}'.format(origin_pt.z) + '\n  ..\n'
+        spc = ''
+        obj_lines = []
+
+        obj_lines.append('"{}" = ROOF'.format(p_name))
+        obj_lines.append('\n   POLYGON           = "{}"'.format(p_name+' Plg'))
+        obj_lines.append('\n   CONSTRUCTION      = "{}_c"'.format(
+            short_name(constr, 30)))
+        obj_lines.append('\n  TILT              =  {}'.format(tilt))
+        obj_lines.append('\n  AZIMUTH           =  {}'.format(azimuth))
+        obj_lines.append('\n  X                 =  {}'.format(origin_pt.x))
+        obj_lines.append('\n  Y                 =  {}'.format(origin_pt.y))
+        obj_lines.append('\n  Z                 =  {}\n  ..\n'.format(origin_pt.z))
+        temp_str = spc.join([line for line in obj_lines])
+
+        doe_windows = [
+            Window(ap, self.face) for ap in self.face.apertures
+        ]
+
+        nl = '\n'
+        if doe_windows is not None:
+            for window in doe_windows:
+                temp_str += window.to_inp() + nl
+            return temp_str
+        else:
+            return temp_str
 
 
 def __repr__(self):
