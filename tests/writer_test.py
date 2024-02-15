f129768b106790dd0dc9ec6d208cc5e6e17f8089
mostaphaRoudsari <sadeghipour@gmail.com>
2024-02-15 09:56:06 -0500

    tests: add test for a model with problematic schedule

--- tests/writer_test.py
+++ tests/writer_test.py
@@ -7,6 +7,7 @@ hvac_test = './tests/assets/multi_hvac.hbjson'
 standard_test = './tests/assets/2023_rac_advanced_sample_project.hbjson'
 air_wall_test = './tests/assets/Air_Wall_test.hbjson'
 
+
 def test_hbjson_translate():
     """Test translating a HBJSON file to an inp file."""
     hb_json = standard_test
@@ -19,6 +20,24 @@ def test_hbjson_translate():
     hb_model = Model.from_file(hb_json)
     honeybee_model_to_inp(hb_model, hvac_mapping='model', exclude_interior_walls=False,
                           folder=out_inp, name='test_model.inp')
+
+    assert out_file.exists()
+    out_file.unlink()
+
+
+def test_hbjson_with_schedule():
+
+    hb_json = './tests/assets/inp_schedule.hbjson'
+    out_inp = './tests/assets/sample_out'
+    out_file = pathlib.Path(out_inp, 'schedule_test.inp')
+    # delete if exists
+    if out_file.exists():
+        out_file.unlink()
+    hb_model = Model.from_file(hb_json)
+    honeybee_model_to_inp(
+        hb_model, hvac_mapping='model', exclude_interior_walls=False,
+        folder=out_inp, name='schedule_test.inp'
+    )
 
     assert out_file.exists()
     out_file.unlink()
