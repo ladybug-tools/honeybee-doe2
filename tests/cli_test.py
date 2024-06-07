42b7dc4077264e5ef733e89aec571d7af8e30bfd
Chris Mackey <chris@ladybug.tools>
2024-06-07 12:45:33 -0700

    fix(tests): Fix several issues discovered while testing

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -1,12 +1,15 @@
 """Test the CLI commands"""
-
+import json
 import os
 from click.testing import CliRunner
 
-from honeybee_doe2.cli.translate import model_to_inp_file
+from honeybee_energy.schedule.ruleset import ScheduleRuleset
+from honeybee_doe2.cli.translate import model_to_inp_file, schedule_from_inp, \
+    schedule_to_inp
 
 
 def test_model_to_inp_cli():
+    """Test the translation of a Model to INP."""
     runner = CliRunner()
     input_hb_model = './tests/assets/shade_test.hbjson'
     out_file = './tests/assets/cli_test.inp'
@@ -21,3 +24,25 @@ def test_model_to_inp_cli():
     assert result.exit_code == 0
     assert os.path.isfile(out_file)
     os.remove(out_file)
+
+
+def test_schedule_to_from_inp():
+    runner = CliRunner()
+    input_hb_sch = './tests/assets/Schedules.inp'
+
+    result = runner.invoke(schedule_from_inp, [input_hb_sch, '--list'])
+    assert result.exit_code == 0
+    result_dict = json.loads(result.output)
+    schedules = [ScheduleRuleset.from_dict(sch) for sch in result_dict]
+    assert len(schedules) >= 36
+
+    output_hb_json = './tests/assets/schedules.json'
+    result = runner.invoke(
+        schedule_from_inp, [input_hb_sch, '--output-file', output_hb_json])
+    assert result.exit_code == 0
+    assert os.path.isfile(output_hb_json)
+
+    result = runner.invoke(schedule_to_inp, [output_hb_json])
+    assert result.exit_code == 0
+
+    os.remove(output_hb_json)
